ea7bdf9c91c4f88ba4c36442132c9b0e5b318222
GN15350 <167824047+GN15350@users.noreply.github.com>
2025-02-20 22:45:27 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Schedulify
+# Untitled
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/GN15350/pen/emYJoYo](https://codepen.io/GN15350/pen/emYJoYo).
+
---
+++ script.js
@@ -0,0 +1,81 @@
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 80,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#00ffff",
+        },
+        "shape": {
+            "color": "#00ffff",
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#00ffff"
+            },
+            "polygon": {
+                "nb_sides": 5
+            },
+        },
+        "opacity": {
+            "color": "#00ffff",
+            "value": 0.5,
+            "random": false,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#00ffff",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 6,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": false,
+        },
+      },
+    },
+    "retina_detect": true,
+  },
+);
